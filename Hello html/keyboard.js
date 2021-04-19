72e66272b362a0aad5c2f97afb9b474c7afe5935
ljxm <lucymurphy98@hotmail.com>
2021-04-19 16:29:10 +0100

    Add files via upload

---
+++ Hello html/keyboard.js
@@ -0,0 +1,284 @@
+function hide(){
+    document.getElementById("collapse").classList.toggle("keyshow");
+}
+
+  function show(param_div_id) {
+    document.getElementById('showemoji').innerHTML = document.getElementById(param_div_id).innerHTML;
+  }
+
+function delete1() {
+    variable = document.getElementById("messagebox").innerHTML;
+    variable = variable.slice(0, -2);
+    document.getElementById("messagebox").innerHTML = variable;
+
+}
+function myFunction1a() {
+    document.getElementById("messagebox").innerHTML += "🤬";
+}
+function myFunction1b() {
+    document.getElementById("messagebox").innerHTML += "😡";
+}
+function myFunction1c() {
+    document.getElementById("messagebox").innerHTML += "😤";
+}
+function myFunction1d() {
+    document.getElementById("messagebox").innerHTML += "😠";
+}
+function myFunction1e() {
+    document.getElementById("messagebox").innerHTML += "😒";
+}
+function myFunction1f() {
+    document.getElementById("messagebox").innerHTML += "😑";
+}
+function myFunction1g() {
+    document.getElementById("messagebox").innerHTML += "🤨";
+}
+function myFunction1h() {
+    document.getElementById("messagebox").innerHTML += "🙄";
+}
+function myFunction1i() {
+    document.getElementById("messagebox").innerHTML += "😐";
+}
+function myFunction2a() {
+    document.getElementById("messagebox").innerHTML += "🤣";
+}
+function myFunction2b() {
+    document.getElementById("messagebox").innerHTML += "😂";
+}
+function myFunction2c() {
+    document.getElementById("messagebox").innerHTML += "😆";
+}
+function myFunction2d() {
+    document.getElementById("messagebox").innerHTML += "🤪";
+}
+function myFunction2e() {
+    document.getElementById("messagebox").innerHTML += "😝";
+}
+function myFunction2f() {
+    document.getElementById("messagebox").innerHTML += "😄";
+}
+function myFunction2g() {
+    document.getElementById("messagebox").innerHTML += "😜";
+}
+function myFunction2h() {
+    document.getElementById("messagebox").innerHTML += "😀";
+}
+function myFunction2i() {
+    document.getElementById("messagebox").innerHTML += "😁";
+}
+function myFunction3a() {
+    document.getElementById("messagebox").innerHTML += "😞";
+}
+function myFunction3b() {
+    document.getElementById("messagebox").innerHTML += "😔";
+}
+function myFunction3c() {
+    document.getElementById("messagebox").innerHTML += "😥";
+}
+function myFunction3d() {
+    document.getElementById("messagebox").innerHTML += "😢";
+}
+function myFunction3e() {
+    document.getElementById("messagebox").innerHTML += "😓";
+}
+function myFunction3f() {
+    document.getElementById("messagebox").innerHTML += "😭";
+}
+function myFunction3g() {
+    document.getElementById("messagebox").innerHTML += "😪";
+}
+function myFunction3h() {
+    document.getElementById("messagebox").innerHTML += "😟";
+}
+function myFunction3i() {
+    document.getElementById("messagebox").innerHTML += "🙁";
+}
+function myFunction4a() {
+    document.getElementById("messagebox").innerHTML += "😯";
+}
+function myFunction4b() {
+    document.getElementById("messagebox").innerHTML += "😮";
+}
+function myFunction4c() {
+    document.getElementById("messagebox").innerHTML += "😲";
+}
+function myFunction4d() {
+    document.getElementById("messagebox").innerHTML += "😧";
+}
+function myFunction4e() {
+    document.getElementById("messagebox").innerHTML += "😳";
+}
+function myFunction4f() {
+    document.getElementById("messagebox").innerHTML += "😦";
+}
+function myFunction4g() {
+    document.getElementById("messagebox").innerHTML += "🤯";
+}
+function myFunction4h() {
+    document.getElementById("messagebox").innerHTML += "🤭";
+}
+function myFunction4i() {
+    document.getElementById("messagebox").innerHTML += "😱";
+}
+function myFunction5a() {
+    document.getElementById("messagebox").innerHTML += "🤒";
+}
+function myFunction5b() {
+    document.getElementById("messagebox").innerHTML += "🤕";
+}
+function myFunction5c() {
+    document.getElementById("messagebox").innerHTML += "🤧";
+}
+function myFunction5d() {
+    document.getElementById("messagebox").innerHTML += "😷";
+}
+function myFunction5e() {
+    document.getElementById("messagebox").innerHTML += "🤢";
+}
+function myFunction5f() {
+    document.getElementById("messagebox").innerHTML += "🤮";
+}
+function myFunction5g() {
+    document.getElementById("messagebox").innerHTML += "🥶";
+}
+function myFunction5h() {
+    document.getElementById("messagebox").innerHTML += "🥵";
+}
+function myFunction5i() {
+    document.getElementById("messagebox").innerHTML += "🥴";
+}
+function myFunction6a() {
+    document.getElementById("messagebox").innerHTML += "😘";
+}
+function myFunction6b() {
+    document.getElementById("messagebox").innerHTML += "😍";
+}
+function myFunction6c() {
+    document.getElementById("messagebox").innerHTML += "🥰";
+}
+function myFunction6d() {
+    document.getElementById("messagebox").innerHTML += "😚";
+}
+function myFunction6e() {
+    document.getElementById("messagebox").innerHTML += "😙";
+}
+function myFunction6f() {
+    document.getElementById("messagebox").innerHTML += "😗";
+}
+function myFunction6g() {
+    document.getElementById("messagebox").innerHTML += "😉";
+}
+function myFunction6h() {
+    document.getElementById("messagebox").innerHTML += "🤩";
+}
+function myFunction6i() {
+    document.getElementById("messagebox").innerHTML += "🤗";
+}
+function myFunction7a() {
+    document.getElementById("messagebox").innerHTML += "😇";
+}
+function myFunction7b() {
+    document.getElementById("messagebox").innerHTML += "😌";
+}
+function myFunction7c() {
+    document.getElementById("messagebox").innerHTML += "😎";
+}
+function myFunction7d() {
+    document.getElementById("messagebox").innerHTML += "😊";
+}
+function myFunction7e() {
+    document.getElementById("messagebox").innerHTML += "🙂";
+}
+function myFunction7f() {
+    document.getElementById("messagebox").innerHTML += "😃";
+}
+function myFunction7g() {
+    document.getElementById("messagebox").innerHTML += "😋";
+}
+function myFunction7h() {
+    document.getElementById("messagebox").innerHTML += "😅";
+}
+function myFunction7i() {
+    document.getElementById("messagebox").innerHTML += "🤠";
+}
+function myFunction8a() {
+    document.getElementById("messagebox").innerHTML += "🤔";
+}
+function myFunction8b() {
+    document.getElementById("messagebox").innerHTML += "😶";
+}
+function myFunction8c() {
+    document.getElementById("messagebox").innerHTML += "🧐";
+}
+function myFunction8d() {
+    document.getElementById("messagebox").innerHTML += "🥱";
+}
+function myFunction8e() {
+    document.getElementById("messagebox").innerHTML += "🤥";
+}
+function myFunction8f() {
+    document.getElementById("messagebox").innerHTML += "🙃";
+}
+function myFunction8g() {
+    document.getElementById("messagebox").innerHTML += "🤐";
+}
+function myFunction8h() {
+    document.getElementById("messagebox").innerHTML += "😕";
+}
+function myFunction8i() {
+    document.getElementById("messagebox").innerHTML += "😴";
+}
+function myFunction9a() {
+    document.getElementById("messagebox").innerHTML += "😣";
+}
+function myFunction9b() {
+    document.getElementById("messagebox").innerHTML += "😩";
+}
+function myFunction9c() {
+    document.getElementById("messagebox").innerHTML += "😖";
+}
+function myFunction9d() {
+    document.getElementById("messagebox").innerHTML += "😰";
+}
+function myFunction9e() {
+    document.getElementById("messagebox").innerHTML += "😫";
+}
+function myFunction9f() {
+    document.getElementById("messagebox").innerHTML += "😬";
+}
+function myFunction9g() {
+    document.getElementById("messagebox").innerHTML += "🥲";
+}
+function myFunction9h() {
+    document.getElementById("messagebox").innerHTML += "😨";
+}
+function myFunction9i() {
+    document.getElementById("messagebox").innerHTML += "😵";
+}
+function myFunction10a() {
+    document.getElementById("messagebox").innerHTML += "🥸";
+}
+function myFunction10b() {
+    document.getElementById("messagebox").innerHTML += "🤫";
+}
+function myFunction10c() {
+    document.getElementById("messagebox").innerHTML += "🥳";
+}
+function myFunction10d() {
+    document.getElementById("messagebox").innerHTML += "🤑";
+}
+function myFunction10e() {
+    document.getElementById("messagebox").innerHTML += "😏";
+}
+function myFunction10f() {
+    document.getElementById("messagebox").innerHTML += "🤤";
+}
+function myFunction10g() {
+    document.getElementById("messagebox").innerHTML += "🥺";
+}
+function myFunction10h() {
+    document.getElementById("messagebox").innerHTML += "🤓";
+}
+function myFunction10i() {
+    document.getElementById("messagebox").innerHTML += "😛";
+}
